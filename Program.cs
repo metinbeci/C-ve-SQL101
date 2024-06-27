8da1f15ce0671c8841bc04343cd686ac895f502a
Code Club <metin.beci@idepex.com>
2024-06-27 22:36:35 +0300

    SQL select-insert-delete-update

--- Program.cs
+++ Program.cs
@@ -16,24 +16,62 @@ namespace Console_Application
         static void Main(string[] args)
         {
             SqlConnection conn = new SqlConnection("Data Source=IDEPEXSRV;Initial Catalog=DbVisit;Integrated Security=True");
-            conn.Open();
-            SqlCommand command= new SqlCommand("Select * from TblPerson",conn);
-            //DataTable dt = new DataTable(); 
-            SqlDataReader reader = command.ExecuteReader(); 
-            while (reader.Read())
+            
+            void listele()
             {
-                Console.WriteLine(reader[0] + "  | " + reader[1] + " | " + reader[2]);
-                Console.WriteLine(".............................................");
+                conn.Open();
+                SqlCommand command = new SqlCommand("Select * from TblPerson", conn);
+                //DataTable dt = new DataTable(); 
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0] + "  | " + reader[1] + " | " + reader[2]);
+                    Console.WriteLine(".............................................");
+                }
+
+
+                conn.Close();
             }
 
 
-            conn.Close();   
+            //--------Ekleme İşlemi-------------------
+
+            //conn.Open();
+            //    SqlCommand command2 = new SqlCommand("insert into TblPerson (Name,City) values (@p1,@p2)",conn);
+            //command2.Parameters.AddWithValue("@p1", "Mesut Gedikli");
+            //command2.Parameters.AddWithValue("@p2", "Samsun");
+            //command2.ExecuteNonQuery();
+
+            //conn.Close() ;  
 
 
+
+
+            //--------Silme İşlemi-------------------
+            //conn.Open();
+            //SqlCommand command3 = new SqlCommand("delete from TblPerson where Id=@p1", conn);
+            //command3.Parameters.AddWithValue("@p1", 3);
+            //command3.ExecuteNonQuery();
+            //conn.Close() ;  
+            //listele();
+
+
+
+            //--------Güncelleme İşlemi-------------------
+            conn.Open();
+            SqlCommand command4 = new SqlCommand("update TblPerson set Name=@p1,City=@p2 where Id=@p3", conn);
+            command4.Parameters.AddWithValue("@p1", "Veli Yıldız");
+            command4.Parameters.AddWithValue("@p2", "Bursa");
+            command4.Parameters.AddWithValue("@p3", 1);
+            command4.ExecuteNonQuery(); 
+            conn.Close() ;  
+
+
+            listele();
             Console.ReadLine();
 
 
         }
     }
 }
-//Data Source=IDEPEXSRV;Initial Catalog=DbVisit;Integrated Security=True
+//Data Source=IDEPEXSRV;Initial Catalog=DbVisit;Int"egrated Security=True
